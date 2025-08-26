25c5deffde1a9000ae3a0a0c72504cf9f17cfb0e
davespser <dav.esp.ser@gmail.com>
2025-08-26 19:13:28 +0200

    Descripción de los cambios

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/metaverso/', // Asegúrate de que esta ruta sea correcta
+  base: '/metaverso/',
 });
